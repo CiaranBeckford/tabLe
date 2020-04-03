cc451b627f8333661919798f2ae7ac254a277d53
selena-huang <sh3696@columbia.edu>
2020-04-03 15:29:41 -0700

    added files from the better microc

--- test2.ml
+++
@@ -1,7 +0,0 @@
-open Sast
-
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Microcparse.program Scanner.token lexbuf in
-  let sprogram = Semant.check program in
-  print_endline (string_of_sprogram sprogram)
